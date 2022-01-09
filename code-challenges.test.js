eb29772fc16cca6e117450d93294b381f300cd14
hawk198723 <hawk198723@gmail.com>
2022-01-08 18:43:01 -0800

    week 4 assessment finished.

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -65,7 +65,7 @@ var nums1 = [3, 56, 90, -8, 0, 23, 6]
 // Expected output: [-8, 90]
 var nums2 = [109, 5, 9, 67, 8, 24]
 // Expected output: [5, 109]
-
+ 
 describe("minAndMax", () => {
     let nums1 = [3, 56, 90, -8, 0, 23, 6]
     let nums2 = [109, 5, 9, 67, 8, 24]
